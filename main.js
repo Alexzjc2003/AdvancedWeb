0bdd274066bd3fa059d455241d58eaf7f265a565
Zhong Jiecheng <21302010025@m.fudan.edu.cn>
2024-02-27 21:27:52 +0800

    camera controls

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 import * as THREE from 'three';
+import { PointerLockControls } from "three/addons/controls/PointerLockControls.js";
+
+import { processInput, onKeyUp, onKeyDown } from './scripts/control.js';
 
 const clock = new THREE.Clock();
 
 
-
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / innerHeight, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
+camera.position.z = 5;
+
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.domElement.tabIndex = 0;
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -17,19 +21,33 @@ const cube = new THREE.Mesh(geometry, material);
 
 scene.add(cube);
 
-camera.position.z = 5;
+
+const PLC = new PointerLockControls(camera, renderer.domElement);
+
+
+
+
+renderer.domElement.addEventListener('keydown', onKeyDown);
+renderer.domElement.addEventListener('keyup', onKeyUp);
+
+
+
+renderer.domElement.addEventListener("click",
+  () => { PLC.lock(); PLC.connect() });
+
+
+document.body.appendChild(renderer.domElement);
+
+
 
 
 const animate = () => {
   // frame logic
   requestAnimationFrame(animate);
+
+
   let deltaTime = clock.getDelta();
-
-  
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
+  processInput(deltaTime, camera);
 
 
   // Draw call
---
+++ scripts/control.js
@@ -0,0 +1,102 @@
+import { Vector3 } from "three";
+
+let movement = {
+  left: false,
+  right: false,
+  up: false,
+  down: false,
+  forward: false,
+  backward: false
+};
+let direction = new Vector3();
+
+
+const onKeyDown = (event) => {
+  console.log(event.code);
+  switch (event.code) {
+    case 'ArrowUp':
+    case 'KeyW':
+      movement.forward = true;
+      break;
+
+    case 'ArrowLeft':
+    case 'KeyA':
+      movement.left = true;
+      break;
+
+    case 'ArrowDown':
+    case 'KeyS':
+      movement.backward = true;
+      break;
+
+    case 'ArrowRight':
+    case 'KeyD':
+      movement.right = true;
+      break;
+
+    case 'Space':
+      movement.up = true;
+      break;
+
+    case 'ShiftLeft':
+      movement.down = true;
+      break;
+  }
+};
+
+const onKeyUp = (event) => {
+  switch (event.code) {
+    case 'ArrowUp':
+    case 'KeyW':
+      movement.forward = false;
+      break;
+
+    case 'ArrowLeft':
+    case 'KeyA':
+      movement.left = false;
+      break;
+
+    case 'ArrowDown':
+    case 'KeyS':
+      movement.backward = false;
+      break;
+
+    case 'ArrowRight':
+    case 'KeyD':
+      movement.right = false;
+      break;
+
+    case 'Space':
+      movement.up = false;
+      break;
+
+    case 'ShiftLeft':
+      movement.down = false;
+      break;
+  }
+};
+
+const processInput = (deltaTime, camera) => {
+  direction.x = Number(movement.right) - Number(movement.left);
+  direction.z = Number(movement.forward) - Number(movement.backward);
+  direction.y = Number(movement.up) - Number(movement.down);
+  direction.normalize();
+  // console.log(direction)
+
+  // camera.translateOnAxis(direction, deltaTime);
+  let velocity = 5;
+  let _dir = camera.getWorldDirection(new Vector3());
+  let _dir_front = new Vector3(_dir.x, 0, _dir.z).normalize();
+  let _dir_up = camera.up.clone().normalize();
+  let _dir_right = new Vector3().crossVectors(_dir, _dir_up).normalize();
+  let _mov = (_dir_front.multiplyScalar(direction.z))
+    .add(_dir_right.multiplyScalar(direction.x))
+    .add(_dir_up.multiplyScalar(direction.y))
+    .multiplyScalar(velocity * deltaTime);
+
+  camera.position.x += _mov.x;
+  camera.position.y += _mov.y;
+  camera.position.z += _mov.z;
+}
+
+export { processInput, onKeyUp, onKeyDown }
