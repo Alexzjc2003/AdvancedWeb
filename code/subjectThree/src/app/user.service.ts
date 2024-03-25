e5a14805772333a55c05d433991f5bdd240c752d
Zhan Meng <21302010039@m.fudan.edu.cn>
2024-03-25 13:15:17 +0800

    miner commit

--- code/subjectThree/src/app/user.service.ts
+++ code/subjectThree/src/app/user.service.ts
@@ -11,28 +11,8 @@ export class UserService {
 
   constructor() { }
 
-  async login(username: string, password: string): Promise<boolean> {
-
-    const postData = {username: username, password: password};
-    try {
-      const response = await fetch(this.loginUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(postData)
-      });
-
-      if (!response.ok) {
-        throw new Error('Login failed');
-      }
-
-      // 在这里可以处理登录成功的逻辑，比如保存用户信息到本地存储等
-      return true;
-    } catch (error) {
-      console.error('Error logging in:', error);
-      return false; // 登录失败
-    }
+  login(username: string, password: string): void{
+    console.log(`${username} -- ${password}`)
   }
 
   register(username: string, password: string): void{
