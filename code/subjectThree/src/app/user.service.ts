279657ccf31de241320b1e2018a85e74b5c97533
Zhan Meng <21302010039@m.fudan.edu.cn>
2024-03-25 12:36:20 +0800

    feat:register_component

--- code/subjectThree/src/app/user.service.ts
+++ code/subjectThree/src/app/user.service.ts
@@ -1,16 +1,38 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  // url = 'http://localhost:3000/locations';
+  loginUrl: string = `${environment.apiUrl}/api/login`
+  registerUrl: string = `${environment.apiUrl}/api/register`
 
   constructor() { }
 
-  login(username: string, password: string): void{
-    console.log(`${username}: ${password}`)
+  async login(username: string, password: string): Promise<boolean> {
+
+    const postData = {username: username, password: password};
+    try {
+      const response = await fetch(this.loginUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postData)
+      });
+
+      if (!response.ok) {
+        throw new Error('Login failed');
+      }
+
+      // 在这里可以处理登录成功的逻辑，比如保存用户信息到本地存储等
+      return true;
+    } catch (error) {
+      console.error('Error logging in:', error);
+      return false; // 登录失败
+    }
   }
 
   register(username: string, password: string): void{
