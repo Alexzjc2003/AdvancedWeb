2fb1e68966f5a68f47b4c85c8e8ff62afc44762c
Zhan Meng <21302010039@m.fudan.edu.cn>
2024-05-16 00:46:57 +0800

    three.js set up

--- code/subjectThree/src/app/main-page/main-page.component.ts
+++ code/subjectThree/src/app/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ThreejsSceneComponent } from '../threejs-scene/threejs-scene.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [ThreejsSceneComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
---
+++ code/subjectThree/src/app/threejs-scene/threejs-scene.component.ts
@@ -0,0 +1,116 @@
+import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+
+@Component({
+  selector: 'app-threejs-scene',
+  standalone: true,
+  imports: [],
+  templateUrl: './threejs-scene.component.html',
+  styleUrl: './threejs-scene.component.css'
+})
+export class ThreejsSceneComponent implements OnInit {
+  scene: THREE.Scene;
+  camera: THREE.PerspectiveCamera;
+  renderer: THREE.WebGLRenderer;
+  model;
+  keys;
+  cameraPosition: THREE.Vector3;
+  lookAtVector: THREE.Vector3;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.initScene();
+    this.renderScene();
+  }
+
+  initScene(): void {
+    // 创建场景
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xadd8e6);
+    
+    // 创建相机
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera.position.y = 2;
+
+    // 创建渲染器
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+
+    let loader = new FBXLoader();
+    this.model = undefined;
+
+    let component = this;
+    
+    loader.load('./assets/model/police.fbx', function (loadedModel) {
+      // 设置模型的初始位置
+      loadedModel.position.set(0, 0, 0);
+      
+      // 添加纹理
+      var textureLoader = new THREE.TextureLoader();
+      textureLoader.load('./assets/model/colormap.png', function (texture) {
+          // 创建基础材质，并设置纹理
+          var material = new THREE.MeshBasicMaterial({ map: texture });
+  
+          // 遍历模型的所有子对象并应用材质
+          loadedModel.traverse(function (child) {
+              if (child instanceof THREE.Mesh) {
+                  child.material = material;
+              }
+          });
+  
+          // 将模型添加到场景中
+          component.scene.add(loadedModel);
+      });
+  
+      component.model = loadedModel;
+      component.model.rotateY(Math.PI);
+  });
+
+    this.cameraPosition = new THREE.Vector3(0, 200, 500);
+    this.lookAtVector = new THREE.Vector3(0, 0, 0);
+
+    let axes = new THREE.AxesHelper(2000);
+    this.scene.add(axes);
+
+    this.keys = [];
+    document.addEventListener('keydown', function(event) {
+        component.keys[event.key] = true;
+    });
+
+    document.addEventListener('keyup', function(event) {
+      component.keys[event.key] = false;
+    });
+  }
+
+  renderScene(): void {
+    const animate = () => {
+      requestAnimationFrame(animate);
+      if(this.model == undefined){
+          return;
+      }
+      
+      if (this.keys['w']) { // W键
+        this.model.position.z -= 30;
+      }
+      if (this.keys['s']) { // S键
+        this.model.position.z += 30;
+      }
+      if (this.keys['a']) { // A键
+        this.model.position.x -= 30;
+      }
+      if (this.keys['d']) { // D键
+        this.model.position.x += 30;
+      }
+
+      this.camera.position.copy(this.cameraPosition.clone().add(this.model.position));
+      this.camera.lookAt(this.lookAtVector.copy(this.model.position));
+      
+      this.renderer.render(this.scene, this.camera);
+    };
+
+    animate();
+  }
+}
