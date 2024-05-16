87e3ebcd1c8a65b91f6212aa98f9a23de7cf46b3
Zhan Meng <21302010039@m.fudan.edu.cn>
2024-05-16 17:28:56 +0800

    car display

--- code/subjectThree/src/app/threejs-scene/threejs-scene.component.ts
+++ code/subjectThree/src/app/threejs-scene/threejs-scene.component.ts
@@ -3,7 +3,6 @@ import * as THREE from 'three';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 @Component({
   selector: 'app-threejs-scene',
@@ -18,14 +17,16 @@ export class ThreejsSceneComponent implements OnInit {
   camera: THREE.PerspectiveCamera;
   renderer: THREE.WebGLRenderer;
   model: THREE.Object3D;
-  road: THREE.Object3D;
+  carDisplay;
+  road;
   keys;
   cameraPosition: THREE.Vector3;
   lookAtVector: THREE.Vector3;
   ambientLight: THREE.AmbientLight;
   directionalLight: THREE.DirectionalLight;
   carBox: THREE.Box3;
-  roadBox: THREE.Box3;
+  carDisplayBox;
+  roadBox;
 
   constructor() { }
 
@@ -40,7 +41,7 @@ export class ThreejsSceneComponent implements OnInit {
     this.scene.background = new THREE.Color(0xadd8e6);
 
     // 创建相机
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5000);
     this.camera.position.y = 2;
 
     // 创建渲染器
@@ -51,15 +52,19 @@ export class ThreejsSceneComponent implements OnInit {
     let loader = new FBXLoader();
     this.model = undefined;
 
-    let component = this;
+    this.carDisplay = [];
+    this.carDisplayBox = [];
+    this.road = [];
+    this.roadBox = [];
 
-    loader.load('./assets/model/police.fbx', function (loadedModel) {
+    let component = this;
+    loader.load('./assets/model/cars/police.fbx', function (loadedModel) {
       // 设置模型的初始位置
       loadedModel.position.set(0, 0, 0);
 
       // 添加纹理
       var textureLoader = new THREE.TextureLoader();
-      textureLoader.load('./assets/model/colormap.png', function (texture) {
+      textureLoader.load('./assets/model/cars/texture/colormap.png', function (texture) {
         // 创建基础材质，并设置纹理
         var material = new THREE.MeshBasicMaterial({ map: texture });
 
@@ -77,22 +82,42 @@ export class ThreejsSceneComponent implements OnInit {
       component.carBox = new THREE.Box3().setFromObject(loadedModel);
       component.model.rotateY(Math.PI);
     });
+    let carNameList = [
+      "ambulance", "box", "cone-flat", "cone", "debris-bolt",
+      "debris-bumper", "debris-door-window", "debris-door", "debris-drivetrain-axle", "debris-drivetrain",
+      "debris-nut", "debris-plate-a", "debris-plate-b", "debris-plate-small-a", "debris-plate-small-b",
+      "debris-spoiler-a", "debris-spoiler-b", "debris-tire", "delivery-flat", "delivery",
+      "firetruck", "garbage-truck", "hatchback-sports", "police", "race-future",
+      "race", "sedan-sports", "sedan", "suv-luxury", "suv",
+      "taxi", "tractor-police", "tractor-shovel", "tractor", "truck-flat",
+      "truck", "van", "wheel-dark", "wheel-default", "wheel-racing",
+      "wheel-tractor-back", "wheel-tractor-dark-back", "wheel-tractor-dark-front", "wheel-tractor-front", "wheel-truck"
+    ];
 
-    let mtlLoader = new MTLLoader();
-    mtlLoader.load('./assets/model/road/roadTile_030.mtl', function (materials) {
-      materials.preload();
-      let objLoader = new OBJLoader();
-      objLoader.setMaterials(materials);
-      objLoader.load('./assets/model/road/roadTile_030.obj', function (object) {
-        object.scale.set(100, 100, 100); // 设置模型的缩放
-        object.position.set(0, 0, -300);
-        component.road = object;
-        component.roadBox = new THREE.Box3().setFromObject(object);
-        component.scene.add(object);
-      });
-    });
+    for(let i = 0; i < carNameList.length; i++){
+      let carName = carNameList[i];
+      let texturePath = "./assets/model/cars/texture/colormap.png";
+      let fbxPath = `./assets/model/cars/${carName}.fbx`;
+      let posX = i * 300;
+      let posZ = 600;
+      this.loadCarDisplay(fbxPath, texturePath, posX, 0, posZ);
+    }
 
-    this.cameraPosition = new THREE.Vector3(0, 200, 500);
+    // 如果不放大，每一块大小为3 * 3
+    for(let i = 1; i < 303; i++){
+      let numStr = i.toString().padStart(3, '0');
+      let mtlPath = `./assets/model/road/roadTile_${numStr}.mtl`;
+      let objPath = `./assets/model/road/roadTile_${numStr}.obj`;
+      let posX = i * 300;
+      let posZ = i % 2 == 0 ? -600: 0;
+      this.loadRoad(mtlPath, objPath, posX, 0, posZ, 100, 100, 100);
+    }
+
+    // 平视
+    // this.cameraPosition = new THREE.Vector3(0, 200, 500);
+    // 俯视
+    this.cameraPosition = new THREE.Vector3(0, 600, 0);
+    
     this.lookAtVector = new THREE.Vector3(0, 0, 0);
 
     // 添加环境光
@@ -123,23 +148,21 @@ export class ThreejsSceneComponent implements OnInit {
       if (this.model == undefined || this.road == undefined) {
         return;
       }
-
+      let speed = 15;
       let deltaX = 0;
       let deltaZ = 0;
 
-      
-
       if (this.keys['w']) { // W键
-        deltaZ = -30;
+        deltaZ = -speed;
       }
       if (this.keys['s']) { // S键
-        deltaZ = 30;
+        deltaZ = speed;
       }
       if (this.keys['a']) { // A键
-        deltaX = -30;
+        deltaX = -speed;
       }
       if (this.keys['d']) { // D键
-        deltaX = 30;
+        deltaX = speed;
       }
 
       this.model.position.x += deltaX;
@@ -148,12 +171,12 @@ export class ThreejsSceneComponent implements OnInit {
       this.carBox.setFromObject(this.model);
 
       // 检查碰撞
-      if (this.roadBox && this.carBox.intersectsBox(this.roadBox)) {
-        console.log('Collision detected!');
-        this.model.position.x -= deltaX;
-        this.model.position.z -= deltaZ;
-        return;
-      }
+      // if (this.roadBox && this.carBox.intersectsBox(this.roadBox)) {
+      //   console.log('Collision detected!');
+      //   this.model.position.x -= deltaX;
+      //   this.model.position.z -= deltaZ;
+      //   return;
+      // }
 
       this.camera.position.copy(this.cameraPosition.clone().add(this.model.position));
       this.camera.lookAt(this.lookAtVector.copy(this.model.position));
@@ -162,5 +185,46 @@ export class ThreejsSceneComponent implements OnInit {
     };
 
     animate();
+  }
+
+  loadRoad(mtlPath, objPath, positionX, positionY, positionZ, scaleX, scaleY, scaleZ){
+    let component = this;
+
+    let mtlLoader = new MTLLoader();
+    mtlLoader.load(mtlPath, function (materials) {
+      materials.preload();
+      let objLoader = new OBJLoader();
+      objLoader.setMaterials(materials);
+      objLoader.load(objPath, function (object) {
+        object.scale.set(scaleX, scaleY, scaleZ);
+        object.position.set(positionX, positionY, positionZ);
+        component.road.push(object);
+        component.roadBox.push(new THREE.Box3().setFromObject(object));
+        component.scene.add(object);
+      });
+    });
+  }
+
+  loadCarDisplay(fbxPath, texturePath, positionX, positionY, positionZ){
+    let component = this;
+    let loader = new FBXLoader();
+    loader.load(fbxPath, function (loadedModel) {
+      loadedModel.position.set(positionX, positionY, positionZ);
+
+      var textureLoader = new THREE.TextureLoader();
+      textureLoader.load(texturePath, function (texture) {
+        var material = new THREE.MeshBasicMaterial({ map: texture });
+        loadedModel.traverse(function (child) {
+          if (child instanceof THREE.Mesh) {
+            child.material = material;
+          }
+        });
+        // 将模型添加到场景中
+        component.scene.add(loadedModel);
+      });
+
+      component.carDisplay.push(loadedModel);
+      component.carDisplayBox.push(new THREE.Box3().setFromObject(loadedModel));
+    });
   }
 }
