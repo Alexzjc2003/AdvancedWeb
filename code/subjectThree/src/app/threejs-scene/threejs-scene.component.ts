d8f2e9cd1accbad2eeb292704704f092ef15076e
WilliamLee <1412638411@qq.com>
2024-05-16 19:32:17 +0800

    use new road kit

--- code/subjectThree/src/app/threejs-scene/threejs-scene.component.ts
+++ code/subjectThree/src/app/threejs-scene/threejs-scene.component.ts
@@ -9,9 +9,8 @@ import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
   standalone: true,
   imports: [],
   templateUrl: './threejs-scene.component.html',
-  styleUrl: './threejs-scene.component.css'
+  styleUrl: './threejs-scene.component.css',
 })
-
 export class ThreejsSceneComponent implements OnInit {
   scene: THREE.Scene;
   camera: THREE.PerspectiveCamera;
@@ -28,7 +27,7 @@ export class ThreejsSceneComponent implements OnInit {
   carDisplayBox;
   roadBox;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.initScene();
@@ -41,7 +40,12 @@ export class ThreejsSceneComponent implements OnInit {
     this.scene.background = new THREE.Color(0xadd8e6);
 
     // 创建相机
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5000);
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      5000
+    );
     this.camera.position.y = 2;
 
     // 创建渲染器
@@ -64,60 +68,178 @@ export class ThreejsSceneComponent implements OnInit {
 
       // 添加纹理
       var textureLoader = new THREE.TextureLoader();
-      textureLoader.load('./assets/model/cars/texture/colormap.png', function (texture) {
-        // 创建基础材质，并设置纹理
-        var material = new THREE.MeshBasicMaterial({ map: texture });
+      textureLoader.load(
+        './assets/model/cars/texture/colormap.png',
+        function (texture) {
+          // 创建基础材质，并设置纹理
+          var material = new THREE.MeshBasicMaterial({ map: texture });
 
-        // 遍历模型的所有子对象并应用材质
-        loadedModel.traverse(function (child) {
-          if (child instanceof THREE.Mesh) {
-            child.material = material;
-          }
-        });
-        // 将模型添加到场景中
-        component.scene.add(loadedModel);
-      });
+          // 遍历模型的所有子对象并应用材质
+          loadedModel.traverse(function (child) {
+            if (child instanceof THREE.Mesh) {
+              child.material = material;
+            }
+          });
+          // 将模型添加到场景中
+          component.scene.add(loadedModel);
+        }
+      );
 
       component.model = loadedModel;
       component.carBox = new THREE.Box3().setFromObject(loadedModel);
       component.model.rotateY(Math.PI);
     });
     let carNameList = [
-      "ambulance", "box", "cone-flat", "cone", "debris-bolt",
-      "debris-bumper", "debris-door-window", "debris-door", "debris-drivetrain-axle", "debris-drivetrain",
-      "debris-nut", "debris-plate-a", "debris-plate-b", "debris-plate-small-a", "debris-plate-small-b",
-      "debris-spoiler-a", "debris-spoiler-b", "debris-tire", "delivery-flat", "delivery",
-      "firetruck", "garbage-truck", "hatchback-sports", "police", "race-future",
-      "race", "sedan-sports", "sedan", "suv-luxury", "suv",
-      "taxi", "tractor-police", "tractor-shovel", "tractor", "truck-flat",
-      "truck", "van", "wheel-dark", "wheel-default", "wheel-racing",
-      "wheel-tractor-back", "wheel-tractor-dark-back", "wheel-tractor-dark-front", "wheel-tractor-front", "wheel-truck"
+      'ambulance',
+      'box',
+      'cone-flat',
+      'cone',
+      'debris-bolt',
+      'debris-bumper',
+      'debris-door-window',
+      'debris-door',
+      'debris-drivetrain-axle',
+      'debris-drivetrain',
+      'debris-nut',
+      'debris-plate-a',
+      'debris-plate-b',
+      'debris-plate-small-a',
+      'debris-plate-small-b',
+      'debris-spoiler-a',
+      'debris-spoiler-b',
+      'debris-tire',
+      'delivery-flat',
+      'delivery',
+      'firetruck',
+      'garbage-truck',
+      'hatchback-sports',
+      'police',
+      'race-future',
+      'race',
+      'sedan-sports',
+      'sedan',
+      'suv-luxury',
+      'suv',
+      'taxi',
+      'tractor-police',
+      'tractor-shovel',
+      'tractor',
+      'truck-flat',
+      'truck',
+      'van',
+      'wheel-dark',
+      'wheel-default',
+      'wheel-racing',
+      'wheel-tractor-back',
+      'wheel-tractor-dark-back',
+      'wheel-tractor-dark-front',
+      'wheel-tractor-front',
+      'wheel-truck',
     ];
 
-    for(let i = 0; i < carNameList.length; i++){
+    for (let i = 0; i < carNameList.length; i++) {
       let carName = carNameList[i];
-      let texturePath = "./assets/model/cars/texture/colormap.png";
+      let texturePath = './assets/model/cars/texture/colormap.png';
       let fbxPath = `./assets/model/cars/${carName}.fbx`;
       let posX = i * 300;
       let posZ = 600;
       this.loadCarDisplay(fbxPath, texturePath, posX, 0, posZ);
     }
 
-    // 如果不放大，每一块大小为3 * 3
-    for(let i = 1; i < 303; i++){
-      let numStr = i.toString().padStart(3, '0');
-      let mtlPath = `./assets/model/road/roadTile_${numStr}.mtl`;
-      let objPath = `./assets/model/road/roadTile_${numStr}.obj`;
+    let roadNameList = [
+      'bridge_pillar',
+      'bridge_pillarWide',
+      'construction_barrier',
+      'construction_light',
+      'construction_pylon',
+      'light_curved',
+      'light_curvedCross',
+      'light_curvedDouble',
+      'light_square',
+      'light_squareCross',
+      'light_squareDouble',
+      'road_bend',
+      'road_bendBarrier',
+      'road_bendSidewalk',
+      'road_bendSquare',
+      'road_bendSquareBarrier',
+      'road_bridge',
+      'road_crossing',
+      'road_crossroad',
+      'road_crossroadBarrier',
+      'road_crossroadLine',
+      'road_crossroadPath',
+      'road_curve',
+      'road_curveBarrier',
+      'road_curveIntersection',
+      'road_curveIntersectionBarrier',
+      'road_curvePavement',
+      'road_drivewayDouble',
+      'road_drivewayDoubleBarrier',
+      'road_drivewaySingle',
+      'road_drivewaySingleBarrier',
+      'road_end',
+      'road_endBarrier',
+      'road_endRound',
+      'road_endRoundBarrier',
+      'road_intersection',
+      'road_intersectionBarrier',
+      'road_intersectionLine',
+      'road_intersectionPath',
+      'road_roundabout',
+      'road_roundaboutBarrier',
+      'road_side',
+      'road_sideBarrier',
+      'road_sideEntry',
+      'road_sideEntryBarrier',
+      'road_sideExit',
+      'road_sideExitBarrier',
+      'road_slant',
+      'road_slantBarrier',
+      'road_slantCurve',
+      'road_slantCurveBarrier',
+      'road_slantFlat',
+      'road_slantFlatCurve',
+      'road_slantFlatHigh',
+      'road_slantHigh',
+      'road_slantHighBarrier',
+      'road_split',
+      'road_splitBarrier',
+      'road_square',
+      'road_squareBarrier',
+      'road_straight',
+      'road_straightBarrier',
+      'road_straightBarrierEnd',
+      'tile_high',
+      'tile_low',
+      'tile_slant',
+      'tile_slantHigh',
+    ];
+
+    for (let i = 0; i < roadNameList.length; i++) {
+      let roadName = roadNameList[i];
+      let mtlPath = `./assets/model/road/${roadName}.mtl`;
+      let objPath = `./assets/model/road/${roadName}.obj`;
       let posX = i * 300;
-      let posZ = i % 2 == 0 ? -600: 0;
-      this.loadRoad(mtlPath, objPath, posX, 0, posZ, 100, 100, 100);
+      let posZ = i % 2 == 0 ? -600 : 0;
+      this.loadRoad(mtlPath, objPath, posX, 0, posZ, 300, 300, 300);
     }
+
+    // 如果不放大，每一块大小为3 * 3
+    // for (let i = 1; i < 303; i++) {
+    //   let numStr = i.toString().padStart(3, '0');
+    //   let mtlPath = `./assets/model/road/roadTile_${numStr}.mtl`;
+    //   let objPath = `./assets/model/road/roadTile_${numStr}.obj`;
+    //   let posX = i * 300;
+    //   let posZ = i % 2 == 0 ? -600 : 0;
+    //   this.loadRoad(mtlPath, objPath, posX, 0, posZ, 100, 100, 100);
+    // }
 
     // 平视
     // this.cameraPosition = new THREE.Vector3(0, 200, 500);
     // 俯视
-    this.cameraPosition = new THREE.Vector3(0, 600, 0);
-    
+    this.cameraPosition = new THREE.Vector3(0, 1200, 0);
+
     this.lookAtVector = new THREE.Vector3(0, 0, 0);
 
     // 添加环境光
@@ -152,16 +274,20 @@ export class ThreejsSceneComponent implements OnInit {
       let deltaX = 0;
       let deltaZ = 0;
 
-      if (this.keys['w']) { // W键
+      if (this.keys['w']) {
+        // W键
         deltaZ = -speed;
       }
-      if (this.keys['s']) { // S键
+      if (this.keys['s']) {
+        // S键
         deltaZ = speed;
       }
-      if (this.keys['a']) { // A键
+      if (this.keys['a']) {
+        // A键
         deltaX = -speed;
       }
-      if (this.keys['d']) { // D键
+      if (this.keys['d']) {
+        // D键
         deltaX = speed;
       }
 
@@ -178,7 +304,9 @@ export class ThreejsSceneComponent implements OnInit {
       //   return;
       // }
 
-      this.camera.position.copy(this.cameraPosition.clone().add(this.model.position));
+      this.camera.position.copy(
+        this.cameraPosition.clone().add(this.model.position)
+      );
       this.camera.lookAt(this.lookAtVector.copy(this.model.position));
 
       this.renderer.render(this.scene, this.camera);
@@ -187,7 +315,16 @@ export class ThreejsSceneComponent implements OnInit {
     animate();
   }
 
-  loadRoad(mtlPath, objPath, positionX, positionY, positionZ, scaleX, scaleY, scaleZ){
+  loadRoad(
+    mtlPath,
+    objPath,
+    positionX,
+    positionY,
+    positionZ,
+    scaleX,
+    scaleY,
+    scaleZ
+  ) {
     let component = this;
 
     let mtlLoader = new MTLLoader();
@@ -205,7 +342,7 @@ export class ThreejsSceneComponent implements OnInit {
     });
   }
 
-  loadCarDisplay(fbxPath, texturePath, positionX, positionY, positionZ){
+  loadCarDisplay(fbxPath, texturePath, positionX, positionY, positionZ) {
     let component = this;
     let loader = new FBXLoader();
     loader.load(fbxPath, function (loadedModel) {
