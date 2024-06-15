96a468b2d6dbdb295b5a9c0c9fc75468dd6d4ef5
WilliamLee <1412638411@qq.com>
2024-06-15 15:05:00 +0800

    add snackbar service fix space css

--- code/subjectThree/src/app/utils/service/snackbar.service.ts
+++ code/subjectThree/src/app/utils/service/snackbar.service.ts
@@ -1,19 +1,49 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SnackbarService {
 
-  constructor(private snackBar: MatSnackBar) { }
+  constructor() { }
 
   showMessage(message: string, panelClass: string): void {
-    let config = new MatSnackBarConfig();
-    config.duration = 3000;
-    config.panelClass = [panelClass]
-    config.horizontalPosition = 'center';
-    config.verticalPosition = 'top';
-    this.snackBar.open(message, "", config);
+    const snackbar = document.createElement('div');
+    snackbar.className = `snackbar ${panelClass}`;
+    snackbar.innerText = message;
+
+    document.body.appendChild(snackbar);
+
+    setTimeout(() => {
+      snackbar.classList.add('show');
+    }, 100);
+
+    setTimeout(() => {
+      snackbar.classList.remove('show');
+      setTimeout(() => {
+        document.body.removeChild(snackbar);
+      }, 300);
+    }, 3000);
   }
 }
+
+
+// import { Injectable } from '@angular/core';
+// import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class SnackbarService {
+
+//   constructor(private snackBar: MatSnackBar) { }
+
+//   showMessage(message: string, panelClass: string): void {
+//     let config = new MatSnackBarConfig();
+//     config.duration = 3000;
+//     config.panelClass = [panelClass]
+//     config.horizontalPosition = 'center';
+//     config.verticalPosition = 'top';
+//     this.snackBar.open(message, "", config);
+//   }
+// }
