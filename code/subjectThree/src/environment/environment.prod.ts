700f9dd460a9e72a0c94edc64147848f8a5639d2
polaris <2673986789@qq.com>
2024-06-20 01:50:38 +0800

    deployment final

--- code/subjectThree/src/environment/environment.prod.ts
+++ code/subjectThree/src/environment/environment.prod.ts
@@ -1,10 +1,10 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    socketUrl: 'ws://10.117.245.17:53000',
-    httpUrl: 'http://10.117.245.17:58080/api',
-    wsUrl: 'ws://10.117.245.17:58080/api',
-    // socketUrl: 'wss://p.jingyijun.xyz',
-    // httpUrl: 'https://p.jingyijun.xyz/api',
-    // wsUrl: 'wss://p.jingyijun.xyz/api',
+    // socketUrl: 'ws://10.117.245.17:53000',
+    // httpUrl: 'http://10.117.245.17:58080/api',
+    // wsUrl: 'ws://10.117.245.17:58080/api',
+    socketUrl: 'wss://p.jingyijun.xyz',
+    httpUrl: 'https://p.jingyijun.xyz/api',
+    wsUrl: 'wss://p.jingyijun.xyz/api',
 };
