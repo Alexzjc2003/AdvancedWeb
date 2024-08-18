e175e0dfca196b7df3238dd0b9258ec4ed96107e
solaris <solaris@solaris-2.local>
2024-08-18 17:17:32 +0800

    update

--- code/subjectThree/src/environment/environment.prod.ts
+++ code/subjectThree/src/environment/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     socketUrl: 'ws://10.117.245.17:53000',
     httpUrl: 'http://10.117.245.17:58080/api',
     wsUrl: 'ws://10.117.245.17:58080/api',
-    // socketUrl: 'wss://p.jingyijun.xyz',
-    // httpUrl: 'https://p.jingyijun.xyz/api',
-    // wsUrl: 'wss://p.jingyijun.xyz/api',
+    // socketUrl: 'wss://ddos.ppolariss.site',
+    // httpUrl: 'https://ddos.ppolariss.site/api',
+    // wsUrl: 'wss://ddos.ppolariss.site/api',
 };
--- code/subjectThree/src/environment/environment.ts
+++ code/subjectThree/src/environment/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:8080/'
-  // apiUrl: 'https://p.jingyijun.xyz/'
+  // apiUrl: 'https://ddos.ppolariss.site/'
   // apiUrl: '10.117.245.17:58080/api',
   socketUrl: 'ws://10.117.245.17:53000',
   httpUrl: 'http://10.117.245.17:58080/api',
   wsUrl: 'ws://10.117.245.17:58080/api',
-  // socketUrl: 'wss://p.jingyijun.xyz',
-  // httpUrl: 'https://p.jingyijun.xyz/api',
-  // wsUrl: 'wss://p.jingyijun.xyz/api',
+  // socketUrl: 'wss://ddos.ppolariss.site',
+  // httpUrl: 'https://ddos.ppolariss.site/api',
+  // wsUrl: 'wss://ddos.ppolariss.site/api',
 };
